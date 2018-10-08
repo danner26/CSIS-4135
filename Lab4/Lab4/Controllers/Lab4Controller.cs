f13dccff8d78316592f19fb55f3eb212b99cbcea
Daniel Anner <daniel.anner@danstechsupport.com>
2018-10-08 09:43:12 -0400

    Lab4 datetime working

--- Lab4/Lab4/Controllers/Lab4Controller.cs
+++ Lab4/Lab4/Controllers/Lab4Controller.cs
@@ -6,15 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace Lab4.Controllers
-{
-    public class Lab4Controller : Controller
-    {
+namespace Lab4.Controllers {
+    public class Lab4Controller : Controller {
         // GET: /<controller>/
-        public IActionResult Index()
-        {
-            ViewData["DateTime"] = DateTime.Now;
+        public IActionResult Index() {
+            ViewData["TimePhrase"] = GetTimePhrase();
+
             return View("Index");
+        }
+        public IActionResult Waffles() {
+            return View("Waffles");
+        }
+
+
+        public string GetTimePhrase() {
+            if (DateTime.Now.Hour < 12) {
+                return "Good Morning";
+            } else if (DateTime.Now.Hour < 18) {
+                return "Good Afternoon";
+            } else {
+                return "Good Evening";
+            }
         }
     }
 }
