584e0d50365165563d37f7960c056e5822acad8b
Daniel Anner <daniel.anner@danstechsupport.com>
2018-10-18 09:24:48 -0400

    Added lab05 and reworked to be names Lab06

---
+++ Lab06/Lab06/Data/lab06Context.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab06.Models
+{
+    public class Lab06Context : DbContext
+    {
+        public Lab06Context (DbContextOptions<Lab06Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab06.Models.Movie> Movie { get; set; }
+    }
+}
