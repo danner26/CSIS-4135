60eb925d1e60c1b4dbaa9b556222fccf09a1ef6b
Daniel Anner <daniel.anner@danstechsupport.com>
2018-10-31 16:23:58 -0400

    Review model setup

---
+++ Lab06/Lab06/Models/Review.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Lab06.Models
+{
+    public class Review
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 2, ErrorMessage = "Name must have at least 2 characters")]
+        [Required]
+        public string Reviewer { get; set; }
+
+        [StringLength(120, MinimumLength = 20, ErrorMessage = "Your review must have at least 20 characters.")]
+        [Required]
+        public string userReview { get; set; }
+
+        [Required]
+        public int movieID { get; set; }
+
+    }
+}
