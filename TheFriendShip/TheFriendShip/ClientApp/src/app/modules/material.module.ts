1bba9a9c1d876bb6f68612d53f9ef7e8d27f5c61
Daniel Anner <daniel.anner@danstechsupport.com>
2018-11-15 09:51:44 -0500

    Almost done!!

--- TheFriendShip/TheFriendShip/ClientApp/src/app/modules/material.module.ts
+++ TheFriendShip/TheFriendShip/ClientApp/src/app/modules/material.module.ts
@@ -1,10 +1,10 @@
-import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatMenuModule } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule],
 })
 
 export class MaterialModule { }
