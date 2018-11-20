f051e849c16eeaa281560deceb1bc4b24798040e
Daniel Anner <daniel.anner@danstechsupport.com>
2018-11-20 09:10:53 -0500

    Adding user context

---
+++ TheFriendShip/TheFriendShip/Data/TFS_UserContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+{
+    public class TFS_UserContext : DbContext
+    {
+        public TFS_UserContext(DbContextOptions<TFS_UserContext> options)
+            : base(options)
+        {
+        }
+    }
+}
