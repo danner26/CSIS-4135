05a5d721b3fc86a1e17e0a2882ddb8fbffc7def4
Greg Walsh <walshg3@go.stockton.edu>
2018-12-02 23:04:28 -0500

    Attempt at getting custom ErrorStateMatcher working

--- TheFriendShip/TheFriendShip/ClientApp/src/app/register/register.component.ts
+++ TheFriendShip/TheFriendShip/ClientApp/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user';
 import { Form, FormsModule } from '@angular/forms';
 import { AuthService } from '../services/auth.service';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 
 @Component({
@@ -31,3 +33,27 @@ export class RegisterComponent implements OnInit {
     }
   }
 }
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+// Tried to get form Validation working... 
+///** @title Input with a custom ErrorStateMatcher */
+//@Component({
+//  selector: 'register.component',
+//  templateUrl: './register.component.html',
+//  styleUrls: ['./register.component.css'],
+//})
+//export class InputErrorStateMatcherExample {
+//  emailFormControl = new FormControl('', [
+//    Validators.required,
+//    Validators.email,
+//  ]);
+
+//  matcher = new MyErrorStateMatcher();
+//}
+
