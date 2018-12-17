65d82dc329017c5c1dd61d67f25bba300c415ae4
Greg Walsh <walshg3@go.stockton.edu>
2018-12-17 00:31:28 -0500

    added IUserRepository interface
    working on getting UserRepository class that implements this interface

---
+++ TheFriendShip/TheFriendShip/Data/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheFriendShip.Data
+{
+    public interface IUserRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        Task<bool> SaveAll();
+        Task<IEnumerable<User>> GetUsers();
+        Task<User> GetUser(int id);
+    }
+}
