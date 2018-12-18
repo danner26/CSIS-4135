3a10b4910b25ed4a0625ae82fb9e1e9509e5ad05
Greg Walsh <walshg3@go.stockton.edu>
2018-12-18 11:03:24 -0500

    Register form is working, need to add functionality still

--- TheFriendShip/TheFriendShip/ClientApp/src/app/modules/material.module.ts
+++ TheFriendShip/TheFriendShip/ClientApp/src/app/modules/material.module.ts
@@ -1,4 +1,4 @@
-import { MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule  } from '@angular/material';
+import { MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule  } from '@angular/material';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -7,8 +7,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule ],
-  exports: [BrowserModule, BrowserAnimationsModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule ],
+  imports: [BrowserModule, BrowserAnimationsModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule  ],
+  exports: [BrowserModule, BrowserAnimationsModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule, MatCheckboxModule, MatToolbarModule, MatFormFieldModule, MatInputModule, MatMenuModule, MatCardModule, MatRadioModule, MatDatepickerModule, MatNativeDateModule  ],
 })
 
 export class MaterialModule { }
